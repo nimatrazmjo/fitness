7bf3dc7c51526bd7f173cf249d5b09913f6d1b1b
Nematullah Razmjo <nimatullah.razmjo@gmail.com>
2016-06-07 11:16:58 +0430

    stylus, add form added

--- index.js
+++ index.js
@@ -9,14 +9,45 @@ var
     express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    stylus = require('stylus'),
     port = 2000;
 
-//--- basic configuration
+/* basic configuration */
 app.set('views',__dirname+'/server/views');
 app.set('view engine','jade');
-
 //---set basic routing for static files
 app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+function compile(str, path) {
+    return stylus(str).set('filename', path);
+}
+app.use(stylus.middleware({
+    src: __dirname+'/public',
+    compile : compile
+}));
+
+/* Routing */
+
+app.get('/add_user', function(req, res) {
+    res.render('profiles/add');
+});
+app.get('/list_user', function(req, res ) {
+   res.render('profiles/list');
+});
+app.post('/user_add',function(req, res, next) {
+   res.send('Under process');
+});
+/* Connect to database*/
+mongoose.connect('mongodb://localhost/fitness');
+var con = mongoose.connection;
+con.once('connected', function () {
+   console.log('Database has been connected to fitness Mongodb');
+});
 
 app.get('*', function(req, res) {
 
